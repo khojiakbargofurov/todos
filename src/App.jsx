fe9c8137b71c154a2e7a99ac6d08b906b031dc4d
Khojiakbar <harry.gafurov@gmail.com>
2024-05-18 12:14:38 +0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer  from "./components/footer/Footer"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <div className="bg-img">
+
+        </div>
+        </Footer>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+//style
+import "./Footer.css"
+
+function Footer() {
+  return ( 
+    <p className="footer">Drag and drop to reorder list</p>
+   );
+}
+
+export default Footer;
